8bb953e78fc8126651b9b85b9239f56c98d4c2ea
Arunima-15 <84953196+Arunima-15@users.noreply.github.com>
2021-06-28 11:05:33 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer()
+{
+  return(
+    <footer>
+  <p>
+    Copyright by SHAPEAI @ {new Date().getFullYear()}
+    </p> 
+    </footer>   
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info()
+{
+  return(
+  <div className="note">
+    <h1>Javascript and React Js</h1>
+    <p>Basic Web Dev React Js Bootcamp</p>
+  </div>    
+  );
+}
+
+export default Info;
